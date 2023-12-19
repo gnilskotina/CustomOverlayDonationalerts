8b54a7e8ccc2abd2ecadca0a3b65c5666d55930d
gnilskotina <62664787+gnilskotina@users.noreply.github.com>
2023-12-19 23:17:38 +0500

    update
    -bugfix
    -add icons
    -style update
    -clear code

--- scripts/alert.js
+++ scripts/alert.js
@@ -9,11 +9,11 @@ let currencylist = {
 }
 fetch('https://www.cbr-xml-daily.ru/daily_json.js') // parse currency
   .then(data => data.json()) //хуево выглядит, потом переделаю...
-  .then (daily_json => {currencylist['EUR'] = daily_json['Valute']['EUR']['Value']/daily_json['Valute']['EUR']['Nominal']
-                        currencylist['USD'] = daily_json['Valute']['USD']['Value']/daily_json['Valute']['USD']['Nominal']
-                        currencylist['TRY'] = daily_json['Valute']['TRY']['Value']/daily_json['Valute']['TRY']['Nominal']
-                        currencylist['BRL'] = daily_json['Valute']['BRL']['Value']/daily_json['Valute']['BRL']['Nominal']
-                        currencylist['PLN'] = daily_json['Valute']['PLN']['Value']/daily_json['Valute']['PLN']['Nominal']})
+  .then (daily_json => {currencylist['EUR'] = daily_json['Valute']['EUR']['Value']/daily_json['Valute']['EUR']['Nominal'];
+                        currencylist['USD'] = daily_json['Valute']['USD']['Value']/daily_json['Valute']['USD']['Nominal'];
+                        currencylist['TRY'] = daily_json['Valute']['TRY']['Value']/daily_json['Valute']['TRY']['Nominal'];
+                        currencylist['BRL'] = daily_json['Valute']['BRL']['Value']/daily_json['Valute']['BRL']['Nominal'];
+                        currencylist['PLN'] = daily_json['Valute']['PLN']['Value']/daily_json['Valute']['PLN']['Nominal'];})
 // config
 let config = {
     "token": localStorage.getItem('token'), // token donationalerts(localstorage or hard code value here)
@@ -35,15 +35,17 @@ let config = {
 
 // functions
 //special img/gif a certain amount (хуево выглядит согласен, потом переделаю...)
-function special(alert,topAlert,username_alert,amount_alert,bottomAlert,otherAlert,amount){
+function special(alert,topAlert,username_alert,icons_alert,amount_alert,bottomAlert,otherAlert,amount){
     switch(true){
         case(amount >= 500): //example alert amount = 500
             sound = config['sounds']['specialSound']; // change sound alert
             otherAlert.src = "./resource/1.jpg"; // !! need other.flag == true !! if ==false alert will not work 
             username_alert.style.backgroundColor = 'gold'; // change top alert backgroundcolor to gold
             username_alert.style.color = 'black'; // change top alert textcolor to gold
-            bottomAlert.style.backgroundColor = 'white' // change bottom alert backgroundcolor to white
-            bottomAlert.style.color = 'black' // change bottom alert textcolor to black
+            icons_alert.style.color = 'gold';
+            amount_alert.style.color = 'gold';
+            bottomAlert.style.backgroundColor = 'white'; // change bottom alert backgroundcolor to white
+            bottomAlert.style.color = 'black'; // change bottom alert textcolor to black
             console.log('special show');
             break;
         
@@ -55,18 +57,18 @@ function speak(txt,alertSoundDuration) {
         let url = 'https://translate.google.com/translate_tts?ie=UTF-8&q='+ txt.replace(" ","%20") +'&tl=ru&client=tw-ob';
         var audio = new Audio(url);
         audio.play();
-        console.log(alertSoundDuration)
+        console.log(alertSoundDuration);
      }, alertSoundDuration*1000)
 }
 // create and show donate alert
-function showdonate(alert_type,username,message,amount,currency,amount2rub)
+function showdonate(data)
 {
-    switch(alert_type){
+    switch(data['alert_type']){
         //donation
         case "1":
         case 1:
             // default sound find
-            sound = config['sounds']['defaultSound']
+            sound = config['sounds']['defaultSound'];
             // create alert form
             var alert = document.createElement("div");
             alert.className = "alert";
@@ -74,24 +76,38 @@ function showdonate(alert_type,username,message,amount,currency,amount2rub)
             // top
             var top_alert = document.createElement("div");
             top_alert.className = "topAlert";
-
+            // username block
             var username_alert = document.createElement("div");
-            username_alert.innerText = username;
-            username_alert.className += config.animation.startAnim
+            username_alert.innerText = data['username'];
+            username_alert.className += config.animation.startAnim;
             username_alert.className += ' usernameAlert';
-
+            // amount block
             var amount_alert = document.createElement("div");
-            amount_alert.innerText = Math.trunc(amount)+' '+currency;
+            amount_alert.innerText = Math.trunc(data['amount'])+' '+data['currency'];
             amount_alert.className += ' amountAlert';
-
-            top_alert.appendChild(amount_alert)
-            top_alert.appendChild(username_alert)
+            // icons block
+            var icons_alert = document.createElement("div");
+            if(data['commission'] == 1){
+                icons_alert.innerText += '♥ ';
+            }
+            if(data['payer_data'] != null){
+                icons_alert.innerText += '✔';
+            }
+            icons_alert.className += 'iconsAlert';
+            // container for icons and amount
+            var container = document.createElement('div');
+            container.style.display = 'flex';
+            container.appendChild(amount_alert);
+            container.appendChild(icons_alert);
+            
+            top_alert.appendChild(container);
+            top_alert.appendChild(username_alert);
             
             alert.appendChild(top_alert);
             // bottom
             var bottom_alert = document.createElement("div");
-            bottom_alert.innerText = message;
-            if (message != ''){ // гавно. временное решение.. потом переделаю...
+            bottom_alert.innerText = data['message'];
+            if (data['message'] != '' & data['message'] != null){ // гавно. временное решение.. потом переделаю...
                 bottom_alert.className = "bottom";
                 bottom_alert.className += " bottomAlert"; // eng - A space is required before the second class. рус - Пробел обязателен перед вторым классом 
                 bottom_alert.className += " " + config.animation.startAnim2;}
@@ -105,12 +121,12 @@ function showdonate(alert_type,username,message,amount,currency,amount2rub)
                 alert.appendChild(other);
             }
             // Update for special alerts
-            special(alert,top_alert,username_alert,amount_alert,bottom_alert,other,amount2rub)
+            special(alert,top_alert,username_alert,icons_alert,amount_alert,bottom_alert,other,data['amount2rub']);
             //playSound
             var audio = new Audio(sound);
             audio.play();
             //speak tts
-            audio.onloadeddata =  function(){speak(message,this.duration)};// audio.onloadeddata find alert sound duration
+            audio.onloadeddata =  function(){speak(data['message'],this.duration)};// audio.onloadeddata find alert sound duration
             // close donation (ебанный костыль. хуй знает как это по-человечьи сделать)
             setTimeout(function(){
                 top_alert.className += " " + config.animation.finishAnim;
@@ -131,16 +147,20 @@ socket.emit('add-user', { token: config.token, type: "alert_widget" });
 
 socket.on('donation',function(msg) {
     pars_msg = JSON.parse(msg);
+    additional_data = JSON.parse(pars_msg['additional_data']);
+    console.log(pars_msg);
+    console.log(additional_data);
     data = {
         alert_type: pars_msg.alert_type,
         username: pars_msg.username,
         message: pars_msg.message,
         amount: pars_msg.amount,
         currency: pars_msg.currency,
-        amount2rub: pars_msg.amount*currencylist[pars_msg.currency]
+        amount2rub: pars_msg.amount*currencylist[pars_msg.currency],
+        commission: additional_data['is_commission_covered'],
+        payer_data: additional_data['payer_data']
     }
     donations.push(data);
-
     console.log(data);
     console.log(donations.length);
 });
@@ -148,15 +168,10 @@ socket.on('donation',function(msg) {
 
 let timer = setInterval(() => {
     if(donations.length != 0){
-        showdonate(donations[0].alert_type,
-            donations[0].username,
-            donations[0].message,
-            donations[0].amount,
-            donations[0].currency,
-            donations[0].amount2rub);
+        showdonate(donations[0]);
         donations.shift();
         console.log('show');
     }
 }, config.duration+3000);
 
-//last update 17.11.23
+//last update 19.12.23
